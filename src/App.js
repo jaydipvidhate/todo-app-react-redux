17eb8613034c58765149a993b28edb82e3ba1889
jaydipvidhate <jaydipvidhate@gmail.com>
2022-03-30 23:50:51 +0530

    title and default state update

--- src/App.js
+++ src/App.js
@@ -1,57 +1,60 @@
-import React,{useState} from 'react';
-import './App.css';
-import { useDispatch,useSelector } from 'react-redux';
-import { addTask, deleteTask } from './action';
+import React, { useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, deleteTask } from "./action";
 
 function App() {
+  const [task, setTask] = useState("");
 
-  const [task, setTask] = useState("")
-
-  const dispatch = useDispatch()
-  const newList = useSelector((data)=>data.list)
+  const dispatch = useDispatch();
+  const newList = useSelector((data) => data.list);
   // console.log(newList);
   const handleSubmit = () => {
     if (!task) {
-      return
+      return alert("Plz fill Task");
     }
-    const key =Math.floor(
-      100000 + Math.random() * 9000,
-    ).toString();
+    const key = Math.floor(100000 + Math.random() * 9000).toString();
     const taskData = {
       id: key,
-      task:task
-    }
-    dispatch(addTask(taskData))
-    setTask('')
-  }
+      task: task,
+    };
+    dispatch(addTask(taskData));
+    setTask("");
+  };
   return (
     <div className="App">
       <h2 className="heading">ToDo List</h2>
-    <div className="inputWrapper">
-     <input type="text"  placeholder="New TODO" value={task} onChange={(e)=>setTask(e.target.value)}/>
-     <button onClick={()=> handleSubmit()}>Add New</button>
-    </div>
-    <div className="buttonsWrapper">
-    <button className="active">All TODO's</button>
-    <button>Active TODO</button>
-    <button>Completed TODO</button>
-
-    </div>
-    <div className="todoListWrapper">
-      {
-        newList!==[] ? newList.map((data,index)=>{
-          return(
-            <div key={data.id} className="todoCardWrapper">
-              <input type="checkbox" name="" id="" />
-      <div  className="todoCard">
-        <h2>{data.task}</h2>
-        <h4 onClick={()=>dispatch(deleteTask(data.id))}>Delete</h4>
-
+      <div className="inputWrapper">
+        <input
+          type="text"
+          placeholder="New TODO"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={() => handleSubmit()}>Add New</button>
       </div>
-            </div>
-        )}) :null
-      }
-    </div>
+      <div className="buttonsWrapper">
+        <button className="active">All TODO's</button>
+        <button>Active TODO</button>
+        <button>Completed TODO</button>
+      </div>
+      <div className="todoListWrapper">
+        {newList !== []
+          ? newList.map((data, index) => {
+              return (
+                <div key={data.id} className="todoCardWrapper">
+                  <input type="checkbox" name="" id="" />
+                  <div className="todoCard">
+                    <h2>{data.task}</h2>
+                    <h4 onClick={() => dispatch(deleteTask(data.id))}>
+                      Delete
+                    </h4>
+                  </div>
+                </div>
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 }
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,31 +1,28 @@
 import { ADDTASK, DELETETASK } from "../action";
 
-
 const initialState = {
-    list:[]
-}
+  list: [{ id: 1234, task: "Wake up early in the morning" }],
+};
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADDTASK:
-            const {id,task} = action.payload
-            return{
-                ...state,
-                list:[...state.list,{id,task}]
-            }
-        case DELETETASK:
+  switch (action.type) {
+    case ADDTASK:
+      const { id, task } = action.payload;
+      return {
+        ...state,
+        list: [...state.list, { id, task }],
+      };
+    case DELETETASK:
+      const newTasks = state.list.filter((task) => task.id !== action.payload);
 
-        const newTasks = state.list.filter((task)=> task.id !== action.payload)
-        
-            return{
-                ...state,
-                list:newTasks
-            }
-    
-        default:
-            return state;
-    } 
-}
+      return {
+        ...state,
+        list: newTasks,
+      };
 
+    default:
+      return state;
+  }
+};
 
-export default reducer
+export default reducer;
